b3076423918c32fa68d37544adc453a1363f2311
Tariq <tariqalamleh1@gmail.com>
2022-12-14 17:55:39 -0500

    learned that all these functions can be contained in one main loop.

--- CarGame.py
+++ CarGame.py
@@ -1,37 +1,27 @@
-while True:
-    command = 0
-    i = 1
-    while command != "quit":
-        command = input(">")
-        i = i + 1
-        if command == "help":
-            print("start - to start car")
-            print("stop - to stop car")
-            print("quit - to exit")
-        elif command == "start":
+command = ""
+stopped = True
+while command != "quit":
+    command = input(">").lower()
+    if command == "help":
+        print("""
+start - to start car
+stop - to stop car
+quit - to exit
+        """)
+    elif command == "start":
+        if stopped == True:
             print("car started!")
-            break
-        elif command == "stop":
-            print("the car is already off...")
-        elif command == "quit":
-            exit()
+            stopped = False
         else:
-            print("function not recognized")
-
-    i = 1
-    while command != "quit":
-        command = input(">")
-        i = i + 1
-        if command == "help":
-            print("start - to start car")
-            print("stop - to stop car")
-            print("quit - to exit")
-        elif command == "start":
+            stopped = False
             print("the car is already running...")
-        elif command == "stop":
-            print("car stopped!")
-            break
-        elif command == "quit":
-            exit()
+    elif command == "stop":
+        if stopped == True:
+            print("the car is already off...")
         else:
-            print("function not recognized")
+            print("car stopped!")
+            stopped = True
+    elif command == "quit":
+        exit()
+    else:
+        print("function not recognized")
